72ba99c44ef62863994da99ea9941aa1fe017c14
Babak Nikanjam <babak.nikanjam@gmail.com>
2019-09-12 20:56:26 -0700

    blog templates

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path, include
 
 from .views_api import PostAPIList, PostAPIDetail
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
+    # PAGES
+    path('', PostListView.as_view(), name='blog_post_list'),
+
+    # API ENDPOINTS
     # ex: blog/api/v1/
     path('api/v1/', PostAPIList.as_view()),
     # ex: blog/api/v1/ab-testing/
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post, Review
 
-# Create your views here.
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostListView(ListView):
+    queryset = Post.objects.all()
+    context_object_name = 'post_list'
+    template_name = 'blog/blog_list.html'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        # context['book_list'] = Book.objects.all()
+        return context
+
+
+class PostCreateView(CreateView):
+    model = Post
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+
+
+class PostDeleteView(DeleteView):
+    pass
+
+
+class ReviewDetailView(DetailView):
+    pass
+
+
+class ReviewListView(ListView):
+    pass
+
+
+class ReviewCreateView(CreateView):
+    pass
+
+
+class ReviewUpdateView(UpdateView):
+    pass
+
+
+class ReviewDeleteView(DeleteView):
+    pass
