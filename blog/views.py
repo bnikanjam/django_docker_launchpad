10296bf130906a1451c15e97811fe7d214af7b42
Babak Nikanjam <babak.nikanjam@gmail.com>
2019-09-13 20:59:10 -0700

    feat: Blog app detail view and template no test yet

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Post, Review
+from django.utils import timezone
 
-# Create your views here.
+
+class PostCreateView(CreateView):
+    model = Post
+
+
+class PostListView(ListView):
+    """A page representing a list of posts"""
+    queryset = Post.objects.all()
+    context_object_name = 'post_list'
+    template_name = 'blog/post_list.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
+
+
+class PostDetailView(DetailView):
+    """A page representing one post"""
+    model = Post
+    context_object_name = 'post'
+    template_name = 'blog/post_detail.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
+
+
+class PostUpdateView(UpdateView):
+    model = Post
+
+
+class PostDeleteView(DeleteView):
+    model = Post
