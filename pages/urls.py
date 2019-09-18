c9c4fa395c2c58d5137783fbf2ad32a6b7f8fecf
Babak Nikanjam <babak.nikanjam@gmail.com>
2019-09-18 07:47:50 -0700

    download pdf

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from pages.views import HomePageView, ContactMeView
+from pages.views import HomePageView, DownloadResumeView
 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='cover'),
-    path('contact-babak/', ContactMeView.as_view(), name='contactme'),
+    path('resume/', DownloadResumeView.as_view(), name='resume'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class HomePageView(TemplateView):
@@ -10,5 +11,5 @@ class AboutPageView(TemplateView):
     template_name = 'about.html'
 
 
-class ContactMeView(TemplateView):
-    template_name = 'contactme.html'
+class DownloadResumeView(LoginRequiredMixin, TemplateView):
+    template_name = 'resume.html'
