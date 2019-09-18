7e98663c5a4f81ffb7d6f17a41e40359d2675f8a
Babak Nikanjam <babak.nikanjam@gmail.com>
2019-09-17 20:40:39 -0700

    feat: _VT Update and Delete views

--- blog/views.py
+++ blog/views.py
@@ -2,19 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post, Review
 from django.utils import timezone
-
-
-class PostCreateView(CreateView):
-    model = Post
-    # context_object_name = 'form'
-    template_name = 'blog/post_create.html'
-    fields = ['title', 'content',]
-    success_url = '/blog/'
-
-    # Set author
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super(PostCreateView, self).form_valid(form)
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 
 class PostListView(ListView):
@@ -41,9 +29,51 @@ class PostDetailView(DetailView):
         return context
 
 
-class PostUpdateView(UpdateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
+    # context_object_name = 'form'
+    template_name = 'blog/post_create.html'
+    fields = ['title', 'content',]
+    success_url = '/blog/'
+
+    # Set author
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super(PostCreateView, self).form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    template_name = 'blog/post_update.html'
+    fields = ['title', 'content']
+
+    # set author
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super(PostUpdateView, self).form_valid(form)
+
+    # Is current post author same as logged-in user?
+    #
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
 
 
 class PostDeleteView(DeleteView):
+    """A view that displays a confirmation page and deletes an existing object. The given object will only be deleted
+    if the request method is POST. If this view is fetched via GET, it will display a confirmation page that should
+    contain a form that POSTs to the same URL. """
     model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = '/blog/'
+
+    # make sure current post author is same as logged-in user
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
