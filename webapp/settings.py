e1e15bbe862053fbb815503c3283675de4a75413
Babak Nikanjam <babak.nikanjam@gmail.com>
2019-09-28 20:09:46 -0700

    Add heroku app URL to ALLOWED_HOSTS

--- webapp/settings.py
+++ webapp/settings.py
@@ -14,7 +14,7 @@ DEBUG = (os.environ.get('DEBUG') == 'True')
 # Deployment Web Security
 if ENVIRONMENT == 'production':
     DEBUG = (os.environ.get('DEBUG') == 'True')
-    ALLOWED_HOSTS = ['https://django-docker-launchpad.herokuapp.com']
+    ALLOWED_HOSTS = ['django-docker-launchpad.herokuapp.com']
     # Help guard against XSS attacks
     SECURE_BROWSER_XSS_FILTER = True
     # Protection against Clickjacking
